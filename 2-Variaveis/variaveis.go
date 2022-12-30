81527e31b2c812d3b2d42e7681a69e52c3f66f25
DayanMonteiro <dmsmamb@gmail.com>
2022-12-30 01:37:22 -0300

    variables and data types

---
+++ 2-Variaveis/variaveis.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var variavel1 string = "Variável 1"
+	variavel2 := "Variável 2"
+	fmt.Println(variavel1)
+	fmt.Println(variavel2)
+
+	var (
+		variavel3 string = "hue"
+		variavel4 string = "hue 2"
+	)
+
+	fmt.Println(variavel3, variavel4)
+
+	variavel5, variavel6 := "Variável 5", "Variável 6"
+
+	fmt.Println(variavel5, variavel6)
+
+	const constante1 string = "Constante 1"
+	fmt.Println(constante1)
+
+	// trocar valor de variáveis
+	variavel5, variavel6 = variavel6, variavel5
+
+	fmt.Println(variavel5, variavel6)
+}
