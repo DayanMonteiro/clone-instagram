879b8b4e47838f11bf94a28d6722bced05263c24
DayanMonteiro <dmsmamb@gmail.com>
2023-01-08 14:47:52 -0300

    routes and controls packages

---
+++ DevBook/api/src/router/rotas/usuarios.go
@@ -0,0 +1,44 @@
+package rotas
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var rotasUsuarios = []Rota{
+	// cadastrar usuário
+	{
+		URI:                "/usuarios",
+		Metodo:             http.MethodPost,
+		Funcao:             controllers.CriarUsuario,
+		RequerAutenticacao: false,
+	},
+	// buscar todos os usuários
+	{
+		URI:                "/usuarios",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarUsuarios,
+		RequerAutenticacao: false,
+	},
+	// buscar um usuário
+	{
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodGet,
+		Funcao:             controllers.BuscarUsuario,
+		RequerAutenticacao: false,
+	},
+	// editar usuário
+	{
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodPut,
+		Funcao:             controllers.AtualizarUsuario,
+		RequerAutenticacao: false,
+	},
+	// deletar usuário
+	{
+		URI:                "/usuarios/{usuarioId}",
+		Metodo:             http.MethodDelete,
+		Funcao:             controllers.DeletarUsuario,
+		RequerAutenticacao: false,
+	},
+}
